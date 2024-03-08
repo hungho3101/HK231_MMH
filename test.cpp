df0bad999ab4d91bf8402391c2f737ab6327c01e
hungho3101 <119468273+hungho3101@users.noreply.github.com>
2024-03-08 23:23:09 +0700

    Create test.cpp

---
+++ test.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main(int argc, const char *argv[])
+{
+    int start = atoi(argv[1]);
+    int end = atoi(argv[2]);
+    cout << start + end;
+    return 0;
+}
